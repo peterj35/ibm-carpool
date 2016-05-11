585632589fb0b035c421c375e1ec2b7359171f75
Peter <peterju35@gmail.com>
2016-05-10 22:27:16 -0400

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | IBM Carpool Buddy"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | IBM Carpool Buddy"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
+    assert_select "title", "Contact | IBM Carpool Buddy"
   end
 
 end
