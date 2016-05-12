ca9c94688d7da50e76af1057362139e928654dbf
Peter <peterju35@gmail.com>
2016-05-12 00:13:48 -0400

    Add Users route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root                  'static_pages#home'
 
   get 'about'       =>  'static_pages#about'
 
   get 'contact'     =>  'static_pages#contact'
+
+  get 'signup'      =>  'users#new'
 
 end
