93215c57bbb7abc32b1ee4cd350399a6e402ed5d
Peter <peterju35@gmail.com>
2016-05-12 22:41:23 -0400

    Completed login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
-  root                  'static_pages#home'
-
-  get 'about'       =>  'static_pages#about'
-
-  get 'contact'     =>  'static_pages#contact'
-
-  get 'signup'      =>  'users#new'
-
+  root                      'static_pages#home'
+  get     'about'       =>  'static_pages#about'
+  get     'contact'     =>  'static_pages#contact'
+  get     'signup'      =>  'users#new'
+  get     'login'       =>  'sessions#new'
+  post    'login'       =>  'sessions#create'
+  delete  'logout'      =>  'sessions#destroy'
   resources :users
 
 end
